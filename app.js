1c9c8d20397d08ae4eefcfe3c4dfd6b6d331ead8
giorgi.b <100768279+giobutiashvili@users.noreply.github.com>
2023-02-15 16:47:52 +0400

    js update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const items = document.querySelectorAll(`.section4 .accordion`);
+const items = document.querySelectorAll('.section4 .accordion');
 
 items.forEach(item => {
-    console.log(item);
-})
+    const trigger = item.querySelector('.section4-div');
+    trigger.addEventListener('click', () => {
+        trigger.nextElementSibling.classList.toggle('active');
+    });
+});
